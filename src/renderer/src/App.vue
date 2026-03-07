3129c610bfe3f3d9d2ef86aae55a47e3e26bb96a
Mak3psh <109795770+Makspsh@users.noreply.github.com>
2026-03-08 02:08:31 +0300

    first commit

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import MenuBar from './components/MenuBar.vue';
+import Tools from './components/Tools.vue';
+import Color from './components/Color.vue';
+import Preview from './components/Preview.vue';
+import Editor from './components/Editor.vue';
+
+</script>
+
+<template>
+
+    <main class="main">
+      <aside class="tools">
+        <Tools />
+      </aside>
+      <section class="selection">
+        <div class="color">
+          <Color />
+        </div>
+        <div class="preview">
+          <Preview />
+        </div>
+      </section>
+      <section class="editor">
+        <Editor />
+      </section>
+    </main>
+
+</template>
+
+<style>
+
+</style>
---
+++ src/renderer/src/components/MenuBar.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>menu bar</div>
+</template>
+
+<style>
+
+</style>
